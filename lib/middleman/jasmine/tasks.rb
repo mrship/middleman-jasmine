42376df5511d18622d8148fbf164f4a6fd854661
Andy Shipman <andy@cllearview.com>
2014-04-10 23:19:54 +0100

    Extract the url to run from the extension

--- lib/middleman/jasmine/tasks.rb
+++ lib/middleman/jasmine/tasks.rb
@@ -4,15 +4,15 @@ require 'json'
 require 'jasmine'
 require 'jasmine/config'
 
-namespace :jasmine do
-
+namespace :middleman_jasmine do
   desc 'Run Jasmine-tests with PhantomJS and print result. Exit with code 1 or 0.'
-  task :ci do
-    config = Jasmine.config
 
-    logger = Logger.new($stdout)
+  task :ci do
+    config       = Jasmine.config
+    logger       = Logger.new($stdout)
     logger.level = Logger::WARN
-    server = Rack::Server.new(:app => Middleman.server, :Port => config.port(:ci), :AccessLog => [], :Logger => logger)
+    server       = Rack::Server.new(:app => Middleman.server, :Port => config.port(:ci), :AccessLog => [], :Logger => logger)
+
     t = Thread.new do
       begin
         server.start
@@ -29,14 +29,14 @@ namespace :jasmine do
     exit_code_formatter = Jasmine::Formatters::ExitCode.new
     formatters << exit_code_formatter
 
-    # FIXME: hardcoded path. should use options sent to Middleman::Jasmine.registered instead
-    path = "/jasmine"
-    url = "#{config.host}:#{config.port(:ci)}#{path}"
+    middleman_extensions = ::Middleman::Application.server.inst.extensions
+    throw "Middleman Jasmine extension not activated" unless middleman_extensions.has_key?(:jasmine)
+
+    path   = middleman_extensions[:jasmine].jasmine_url
+    url    = "#{config.host}:#{config.port(:ci)}#{path}"
     runner = config.runner.call(Jasmine::Formatters::Multi.new(formatters), url)
     runner.run
 
     abort unless exit_code_formatter.succeeded?
   end
-
 end
-
